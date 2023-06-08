1cd59b55b340cb215a0c2d2afcd0401e29fadf71
Mathieu Lamarque <math.lamarque@gmail.com>
2023-06-08 19:25:01 +0200

    Force https for redirect_uri

--- microsoft_auth/utils.py
+++ microsoft_auth/utils.py
@@ -4,16 +4,17 @@ from .conf import HOOK_SETTINGS, config as global_config
 
 
 def get_scheme(request, config=None):
-    if config is None:
-        config = global_config
+#     if config is None:
+#         config = global_config
 
-    scheme = "https"
-    if config.DEBUG and request is not None:
-        if "HTTP_X_FORWARDED_PROTO" in request.META:
-            scheme = request.META["HTTP_X_FORWARDED_PROTO"]
-        else:
-            scheme = request.scheme
-    return scheme
+#     scheme = "https"
+#     if config.DEBUG and request is not None:
+#         if "HTTP_X_FORWARDED_PROTO" in request.META:
+#             scheme = request.META["HTTP_X_FORWARDED_PROTO"]
+#         else:
+#             scheme = request.scheme
+#     return scheme
+    return 'https'
 
 
 def get_hook(name):
